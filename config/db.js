692605f373ef02cd484270fb67ccca47293f650a
zcas12 <zcad8546@naver.com>
2025-02-20 18:20:43 +0900

    db config 수정

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
-
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: '.env.local' }); // .env.local 명시적으로 지정
+}
 const dbConfig = {
     host: "centerbeam.proxy.rlwy.net",
     port: 13139,
     user: "root",
-    password: process.env.DB_PASSWORD || 'RopKTAcsXCQgaWbpiRXkxuXFIkZOKbeb', // 비밀번호만 환경 변수 사용
+    password: process.env.DB_PASSWORD , // 비밀번호만 환경 변수 사용
     database: "weholder",
     waitForConnections: true,
     connectionLimit: 30
