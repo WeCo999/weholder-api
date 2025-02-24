9561049131ce1fd8caf9315fd995856778045f44
leejh <zcad8546@naver.com>
2025-02-24 13:49:36 +0900

    메인 병렬 호출 방식으로 변경

--- routes/main/index.js
+++ routes/main/index.js
@@ -7,21 +7,24 @@ router.get('/', async(req,res)=>{
         const wemadeCodes = ['expect', 'worry'];
         const coinCodes = ['kleva', 'kroma', 'cross', 'etcCoin'];
         const gameCodes = ['lostSword', 'ymir', 'nightCrows', 'mir4', 'mir5', 'war', 'etcGame'];
-        const humor = await Main.findMainBoardByCode('humor');
-        const wemade = await Main.findMainBoardByCodes(wemadeCodes);
-        const coin = await Main.findMainBoardByCodes(coinCodes);
-        const game = await Main.findMainBoardByCodes(gameCodes);
-        const politics = await Main.findMainBoardByCode('politics');
-        const discussion = await Main.findMainBoardByCode('discussion');
 
-        const result ={
+        const [humor, wemade, coin, game, politics, discussion] = await Promise.all([
+            Main.findMainBoardByCode('humor'),
+            Main.findMainBoardByCodes(wemadeCodes),
+            Main.findMainBoardByCodes(coinCodes),
+            Main.findMainBoardByCodes(gameCodes),
+            Main.findMainBoardByCode('politics'),
+            Main.findMainBoardByCode('discussion')
+        ]);
+
+        const result = {
             wemadeList: wemade,
             coinList: coin,
             gameList: game,
             politicsList: politics,
             discussionList: discussion,
             humorList: humor
-        }
+        };
         res.status(200).json({resultCd:"200", resultMsg: "조회성공", resultData: result });
     }catch (e) {
         console.log(e)
