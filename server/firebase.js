c4755f8aa86c8d45aa962f85c20bb7d97ca6b078
zcas12 <zcad8546@naver.com>
2025-02-25 00:20:04 +0900

    이미지 업로드 기능추가 TEST

--- server/firebase.js
+++ server/firebase.js
@@ -1,5 +1,20 @@
+require('dotenv').config(); // dotenv 설정
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // Firebase 콘솔에서 다운로드한 JSON
+
+// 환경변수로부터 비밀키를 가져옴
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // \n이 문자열에 포함되어 있을 수 있기 때문에 수정
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40weholder-49cfc.iam.gserviceaccount.com",
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
