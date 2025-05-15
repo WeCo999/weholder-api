4e9e0b783cd46b8e377ebd2c7d750ed873ce2b9e
leejh <zcad8546@naver.com>
2025-05-15 12:50:45 +0900

    db 확인

--- database/mysql.js
+++ database/mysql.js
@@ -8,10 +8,19 @@ function getConnection() {
             if (err) {
                 reject(new Error('Error connecting to the database: ' + err.message));
             } else {
-                resolve(conn);
+                // 세션 타임존 설정
+                conn.query("SET time_zone = 'Asia/Seoul'", (err) => {
+                    if (err) {
+                        conn.release(); // 커넥션은 반드시 반환
+                        reject(new Error('Failed to set time zone: ' + err.message));
+                    } else {
+                        resolve(conn); // 설정 성공 시 커넥션 반환
+                    }
+                });
             }
         });
     });
 }
+
 
 module.exports = getConnection;
